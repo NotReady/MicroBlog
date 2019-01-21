31415fdc184d834118694fe39738961de80c0bf9
notready <loreley.lala@gmail.com>
2019-01-21 16:07:15 +0900

    ビュークラスの実装

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
+# 汎用ビュー リストビューのインポート
+from django.views.generic import ListView
+# 実装モデルのインポート
+from .models import Blog
 
+class BlogListView(ListView):
+    model = Blog
