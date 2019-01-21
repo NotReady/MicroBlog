fc2b3a4fc5b83a851c069d6ba6f2ee498acd794a
notready <loreley.lala@gmail.com>
2019-01-21 17:16:14 +0900

    DetailViewクラスの実装

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-# 汎用ビュー リストビューのインポート
+# クラスベースビュー リストビューのインポート
 from django.views.generic import ListView
+# クラスベースビュー 詳細ビューのインポート
+from django.views.generic import DetailView
+
 # 実装モデルのインポート
 from .models import Blog
 
+# リストビュー
 class BlogListView(ListView):
+    model = Blog
+
+# 詳細ビュー
+class BlogDetailView(DetailView):
     model = Blog
