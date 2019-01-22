a65411ee668e06c4ae3d87c291c86846da21d699
notready <loreley.lala@gmail.com>
2019-01-22 22:05:45 +0900

    bootstrapの適応

--- microblog/microblog/settings.py
+++ microblog/microblog/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# プロジェクトフォルダ
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# 静的ファイルの場所
+STATICFILES_DIRS = (
+    # BASE_DIR
+    os.path.normpath(os.path.join(BASE_DIR, "assets")),
+)
