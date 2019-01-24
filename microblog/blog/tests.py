90510e4b18ba227f836d8016e8a6af8f2f3572f2
notready <loreley.lala@gmail.com>
2019-01-24 23:50:08 +0900

    単体テストを実装

--- microblog/blog/tests.py
+++ microblog/blog/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+
+    # @override テスト前
+    def setUp(self):
+        # テストクライアントを作成
+        self.c = Client()
+
+    # python manage.py test でテスト実行
+    # 以下のテストケース関数をシーケンス的に実行
+
+    # ページにアクセス成功するか
+    def test_index_access(self):
+        # クライアントを使ってルートにアクセス
+        res = self.c.get('/')
+
+        # レスポンスのステータスコードを検証
+        # status code => 200 OK
+        # status code => 404 Not Fount
+        # status code => 302 Redirect
+        self.assertEqual(200, res.status_code)
+
+    # 存在しないページがNotFountとなるか
+    def test_fail_access(self):
+        res = self.c.get('/unknow')
+        self.assertEqual(404, res.status_code)
